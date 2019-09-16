f599bd649aa6825d12c838c73189f9b0e8acf558
Fabian Krueger <fabian.krueger@fau.de>
2019-09-16 14:43:43 +0200

    db: created model for client

--- store/models.py
+++ store/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 
-class user(models.model):
+class user(models.Model):
     firstname = models.TextField()
     surname = models.TextField()
     nickname = models.TextField()
     money = models.DecimalField(max_digits=6, decimal_places=2)
+
+class client(models.Model):
+    mac = models.TextField(primary_key=True)
