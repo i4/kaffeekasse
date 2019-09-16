7337cf12bd08468ef14272ec96e28d1a6365c4a4
Fabian Krueger <fabian.krueger@fau.de>
2019-09-16 15:54:17 +0200

    crate model for transfers

--- store/models.py
+++ store/models.py
@@ -30,3 +30,13 @@ class purchase(models.Model):
     client_id = models.ForeignKey('client', on_delete=models.CASCADE, null=False)
     time_stamp = models.TimeField(null=False, auto_now=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class transfer(models.Model):
+    id = models.TextField(primary_key=True)
+    sender_id = models.ForeignKey('user', on_delete=models.SET_NULL, null=True,
+            related_name='sender_id')
+    receiver_id = models.ForeignKey('user', on_delete=models.SET_NULL,
+            null=True, related_name='receiver_id')
+    client_id = models.ForeignKey('client', on_delete=models.SET_NULL, null=True)
+    time_stamp = models.TimeField(null=False, auto_now=True)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
