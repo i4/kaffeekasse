ce194f6d017a8c899a9be06a9bb634de6f0e63c7
Fabian Krueger <fabian.krueger@fau.de>
2019-09-16 15:28:15 +0200

    db: create model for charge

--- store/models.py
+++ store/models.py
@@ -15,3 +15,10 @@ class product(models.Model):
     category = models.TextField(null=False)
     stock = models.IntegerField(null=False)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class charge(models.Model):
+    id = models.TextField(primary_key=True)
+    user_id = models.ForeignKey('user', on_delete=models.CASCADE, null=False)
+    client_id = models.ForeignKey('client', on_delete=models.CASCADE, null=False)
+    time_stamp = models.TimeField(null=False, auto_now=True)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
