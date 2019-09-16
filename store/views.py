e297b883874792b167184b983a5fa52cde8fa1e0
Lukas Schneider <lukas.s.schneider@fau.de>
2019-09-16 14:43:10 +0200

    Add view handlers in views.py

--- store/views.py
+++ store/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "index.html", {})
+
+
+def buy(request):
+    return render(request, "buy.html", {})
+
+
+def charge(request):
+    return render(request, "charge.html", {})
+
+
+def transfer(request):
+    return render(request, "transfer.html", {})
