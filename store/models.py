34037c1b06ede0da845abda66d8e62ba97cdc726
Fabian Krueger <fabian.krueger@fau.de>
2019-09-16 14:25:35 +0200

    db: created model for user

--- store/models.py
+++ store/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class user(models.model):
+    firstname = models.TextField()
+    surname = models.TextField()
+    nickname = models.TextField()
+    money = models.DecimalField(max_digits=6, decimal_places=2)
