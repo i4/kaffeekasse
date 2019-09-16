d82c31d3d58018378b9dc0b6ead4dfa654efee6b
Lukas Schneider <lukas.s.schneider@fau.de>
2019-09-16 13:38:08 +0200

    Add store/urls.py with the basic urls for the website

---
+++ store/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('buy', views.buy, name='buy'),
+    path('transfer', views.transfer, name='transfer'),
+    path('charge', views.charge, name="charge"),
+]
