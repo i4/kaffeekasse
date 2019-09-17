1593c9068274c66d5eade7846a7238082bd1922f
Lukas Schneider <lukas.s.schneider@fau.de>
2019-09-17 15:47:17 +0200

    Add login functionality

--- store/urls.py
+++ store/urls.py
@@ -3,9 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Views
     path('', views.index, name='index'),
     path('buy', views.buy, name='buy'),
     path('transfer', views.transfer, name='transfer'),
     path('charge', views.charge, name="charge"),
+
+    # Auth
+    path('login', views.login, name="login"),
+
+    # Test
     path('test', views.test, name="test"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from .models import User, Product
 from django.views.decorators.http import require_http_methods
-from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import login
+from .backend import *
 
 # Create your views here.
 
@@ -25,6 +27,15 @@ def charge(request):
 @require_http_methods(["GET"])
 def transfer(request):
     return render(request, "transfer.html", {"Users": User.objects.all()})
+
+@require_http_methods(["POST"])
+@csrf_protect
+def login(request):
+    if (LoginLogic.login(request, request.POST.get("user_id"))):
+        return HttpResponseRedirect(reverse("buy"))
+    else:
+        return HttpResponseRedirect(reverse("index"))
+
 
 def test(request):
     user = User.objects.get(pk=1)
