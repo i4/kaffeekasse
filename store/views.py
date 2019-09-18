cb02ac4e2a17c02299c3377adf5caae8d5684af3
Lukas Schneider <lukas.s.schneider@fau.de>
2019-09-18 08:58:20 +0200

    pip8 fixes

--- store/views.py
+++ store/views.py
@@ -13,12 +13,14 @@ from .backend import *
 def index(request):
     return render(request, "index.html", {"Users": User.objects.all()})
 
+
 @require_http_methods(["GET"])
 def buy(request):
     return render(request, "buy.html", {
         "drinks": Product.objects.filter(category="drink"),
         "candies": Product.objects.filter(category="candy"),
     })
+
 
 @require_http_methods(["GET"])
 def charge(request):
@@ -51,7 +53,7 @@ def logout(request):
 def test(request):
     return render(request, "test.html", {})
 
+
 def test2(request):
     GetMostFrequentUsers.getFrequentUsersList("0x50xd-0xf0x3-0x60xd-0x50x3-0xd0x9-0xb0x20x50x4-0x90x2-0xd0xd-0x20x3-0xa0x9-0x10x00x80x4-0x50x8-0xf0x6-0xb0xd-0x50xb-0x90xf",
-            4, 4*7)
-
+                                              4, 4*7)
