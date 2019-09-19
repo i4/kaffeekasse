aca9bd911b545d0b45344ce28a3cd1b50b97029f
Fabian Krueger <fabian.krueger@fau.de>
2019-09-19 09:09:11 +0200

    add config for all global parameters used in store

---
+++ store/store_config.py
@@ -0,0 +1,12 @@
+"""
+N_LAST_BOUGHT_PRODUCTS: number of products to be shown in 'Letzte Einkäufe'
+T_LAST_BOUGHT_PRODUCTS_D: number of days in the past that are used as time intervall limit to search for 'Letzte Einkäufe'
+T_ANNULLABLE_PURCHASE_M: time of minutes a user has for a purchase to undo it
+N_MOST_BOUGHT_PRODUCTS: number of products to be shown in 'Häufig gekauft'
+T_MOST_BOUGHT_PRODUCTS_D: number of days in the past that are used as time intervall limit to search for 'Häufig gekauft'
+"""
+KAFFEEKASSE = {'N_LAST_BOUGHT_PRODUCTS': 5,
+               'T_LAST_BOUGHT_PRODUCTS_D': 30
+               'T_ANNULLABLE_PURCHASE_M': 60}
+               'N_MOST_BOUGHT_PRODUCTS': 5,
+               'T_MOST_BOUGHT_PRODUCTS_D': 30,
