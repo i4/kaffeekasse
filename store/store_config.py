4c47fa7847718c1b7995285f2097cb423928de78
Fabian Krueger <fabian.krueger@fau.de>
2019-09-19 09:09:11 +0200

    add config for all global parameters used in store

---
+++ store/store_config.py
@@ -0,0 +1,16 @@
+"""
+N_LAST_BOUGHT_PRODUCTS: number of products to be shown in 'Letzte Einkäufe'
+T_LAST_BOUGHT_PRODUCTS_D: number of days in the past that are used as time intervall limit to search for 'Letzte Einkäufe'
+T_ANNULLABLE_PURCHASE_M: time of minutes a user has for a purchase to undo it
+N_MOST_BOUGHT_PRODUCTS: number of products to be shown in 'Häufig gekauft'
+T_MOST_BOUGHT_PRODUCTS_D: number of days in the past that are used as time intervall limit to search for 'Häufig gekauft'
+N_USERS_LOGIN: number of users that should be shown in the users list in the login-screen. A negative number means
+     'all users are selected'
+"""
+KAFFEEKASSE = {'N_LAST_BOUGHT_PRODUCTS': 5,
+               'T_LAST_BOUGHT_PRODUCTS_D': 30
+               'T_ANNULLABLE_PURCHASE_M': 60
+               'N_MOST_BOUGHT_PRODUCTS': 5,
+               'T_MOST_BOUGHT_PRODUCTS_D': 30,
+               'N_USERS_LOGIN': -1,
+               }
