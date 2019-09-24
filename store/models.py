106066b4f9bbe4caa91fe6172a575739ab6c52fa
Fabian Krueger <fabian.krueger@fau.de>
2019-09-24 08:46:35 +0200

    update models: user

--- store/models.py
+++ store/models.py
@@ -9,7 +9,7 @@ from .store_exceptions import *
 class User(AbstractUser):
     username = models.TextField(null=False, unique=True)
     password = models.TextField(null=True)
-    email = None
+    email = models.EmailField(null=True, blank=True)
     money = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
 
     def incrementMoney(self, amount):
