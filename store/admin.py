1d6c5f470ed7194f70f25f4e794a973262c22342
Simon Ruderich <ruderich@cs.fau.de>
2019-11-13 12:53:59 +0100

    admin: improve displayed lists

--- store/admin.py
+++ store/admin.py
@@ -5,31 +5,32 @@ import store.models as models
 
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'first_name',
-                    'last_name', 'is_active', 'is_staff', 'money')
+    list_display = ('username', 'first_name', 'last_name', 'email', 'money',
+            'pk_login_enabled', 'is_active', 'is_staff', 'is_superuser')
+
     fieldsets = (
         (None, {
             'fields': ('username', 'first_name', 'last_name', 'pk_login_enabled', 'money')
         }),
     )
-    pass
+
 
 @admin.register(models.UserIdentifier)
 class UserIdentifierAdmin(admin.ModelAdmin):
-    list_display = ('id', 'identifier', 'identifier_type', 'user')
-    fields = ('identifier', 'identifier_type', 'user')
+    list_display = ('user', 'identifier_type', 'identifier')
+
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category', 'stock')
-    fields = ('name', 'price', 'category', 'stock')
+    list_display = ('name', 'category', 'price', 'stock')
+
 
 @admin.register(models.ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'toplevel', 'sublevel')
-    fields = ('toplevel', 'sublevel')
+    list_display = ('toplevel', 'sublevel')
+    list_display_links = ('sublevel',)
+
 
 @admin.register(models.ProductIdentifier)
 class ProductIdentifierAdmin(admin.ModelAdmin):
-    list_display = ('id', 'identifier', 'identifier_type', 'product')
-    fields = ('identifier', 'identifier_type', 'product')
+    list_display = ('product', 'identifier_type', 'identifier')
