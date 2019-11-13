4e502bbef5f358549f1c2407cf8c0c9865819b79
Simon Ruderich <ruderich@cs.fau.de>
2019-11-13 14:42:06 +0100

    models: remove unused imports

--- store/models.py
+++ store/models.py
@@ -1,6 +1,5 @@
 from django.db import models, IntegrityError
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils import timezone
 from decimal import Decimal
 from .store_exceptions import *
