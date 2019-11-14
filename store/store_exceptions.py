acf6561f35c7afd8c930527f6044bfb33f1aa0ba
Simon Ruderich <ruderich@cs.fau.de>
2019-11-14 18:50:15 +0100

    store_exceptions: use base class for all our exceptions

--- store/store_exceptions.py
+++ store/store_exceptions.py
@@ -1,48 +1,52 @@
-class UserNotEnoughMoney(Exception):
+class ClientMessageException(Exception):
+    """An exception which informs the client about an error."""
+
+
+class UserNotEnoughMoney(ClientMessageException):
     def __init__(self):
         super().__init__("Guthaben reicht nicht aus!")
 
 
-class NegativeMoneyAmount(Exception):
+class NegativeMoneyAmount(ClientMessageException):
     def __init__(self):
         super().__init__("Positiver Betrag erwartet!")
 
 
-class NotAnnullable(Exception):
+class NotAnnullable(ClientMessageException):
     def __init__(self):
         super().__init__("Nicht annullierbar!")
 
 
-class PurchaseNotAnnullable(Exception):
+class PurchaseNotAnnullable(ClientMessageException):
     def __init__(self):
         super().__init__("Einkauf ist nicht annullierbar!")
 
 
-class ChargeNotAnnullable(Exception):
+class ChargeNotAnnullable(ClientMessageException):
     def __init__(self):
         super().__init__("Aufladung ist nicht annullierbar!")
 
 
-class TransferNotAnnullable(Exception):
+class TransferNotAnnullable(ClientMessageException):
     def __init__(self):
         super().__init__("Überweisung ist nicht annullierbar!")
 
 
-class UserIdentifierNotExists(Exception):
+class UserIdentifierNotExists(ClientMessageException):
     def __init__(self):
         super().__init__("Kein Nutzer unter der Identifikationsnummer registriert!")
 
 
-class DisabledIdentifier(Exception):
+class DisabledIdentifier(ClientMessageException):
     def __init__(self):
         super().__init__("Gewünschte Art der Identifizierung vom Nutzer gesperrt!")
 
 
-class ProductIdentifierNotExists(Exception):
+class ProductIdentifierNotExists(ClientMessageException):
     def __init__(self):
         super().__init__("Kein Produkt unter der Identifikationsnummer registriert!")
 
 
-class SenderEqualsReceiverError(Exception):
+class SenderEqualsReceiverError(ClientMessageException):
     def __init__(self):
         super().__init__("Absender darf nicht auch der Adressat sein!")
