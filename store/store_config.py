b6ce13d1f33c8b3d7c113d02a8059c6de7868a15
Simon Ruderich <ruderich@cs.fau.de>
2019-12-02 13:00:46 +0100

    store_config: group settings

--- store/store_config.py
+++ store/store_config.py
@@ -3,23 +3,26 @@
 KAFFEEKASSE = {
     # Number of products to be shown in 'Letzte Einkäufe'
     'N_LAST_BOUGHT_PRODUCTS': 100,
-    # Time of minutes a user has for a purchase to undo it
-    'T_ANNULLABLE_PURCHASE_M': 60,
     # number of products to be shown in 'Häufig gekauft'
     'N_MOST_BOUGHT_PRODUCTS': 10,
     # Number of days in the past that are used as time intervall limit to
     # search for 'Häufig gekauft'
     'T_MOST_BOUGHT_PRODUCTS_D': 30,
+
     # Number of charges that should be shown in 'Letzte Aufladungen'. A
     # negative number means 'all users are selected'
     'N_LAST_CHARGES': 10,
+
+    # Time of minutes a user has for a purchase to undo it
+    'T_ANNULLABLE_PURCHASE_M': 60,
     # Time in minutes a user has for a charge to undo it
     'T_ANNULLABLE_CHARGE_M': 60,
+    # Time in minutes a user has for a transfer to undo it
+    'T_ANNULLABLE_TRANSFERS_M': 60,
+
     # Number of receivers that should be shown in 'alle Empfänger'. A negative
     # value means 'all users are shown'
     'N_TRANSFERS_RECEIVERS': -1,
     # Number of transfers that should be shown in 'Letzte Überweisungen'
     'N_LAST_TRANSFERS': 10,
-    # Time in minutes a user has for a transfer to undo it
-    'T_ANNULLABLE_TRANSFERS_M': 60,
 }
