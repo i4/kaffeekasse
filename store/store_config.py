7e5637ff2bd2e4701cf5dc3cc6d6424d9863bdd5
Simon Ruderich <ruderich@cs.fau.de>
2019-12-02 12:58:01 +0100

    store_config: reduce duplication in documentation

--- store/store_config.py
+++ store/store_config.py
@@ -1,26 +1,25 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-"""
-N_LAST_BOUGHT_PRODUCTS: number of products to be shown in 'Letzte Einkäufe'
-T_ANNULLABLE_PURCHASE_M: time of minutes a user has for a purchase to undo it
-N_MOST_BOUGHT_PRODUCTS: number of products to be shown in 'Häufig gekauft'
-T_MOST_BOUGHT_PRODUCTS_D: number of days in the past that are used as time intervall limit to search for 'Häufig gekauft'
-N_LAST_CHARGES: number of charges that should be shown in 'Letzte Aufladungen'. A negative number means 'all users are
-    selected'
-T_ANNULLABLE_CHARGE_M: time in minutes a user has for a charge to undo it
-N_TRANSFERS_RECEIVERS: number of receivers that should be shown in 'alle Empfänger'. A negative value means 'all
-users are shown'
-N_LAST_TRANSFERS: number of transfers that should be shown in 'Letzte Überweisungen'
-T_ANNULLABLE_TRANSFERS_M: time in minutes a user has for a transfer to undo it
-"""
 KAFFEEKASSE = {
+    # Number of products to be shown in 'Letzte Einkäufe'
     'N_LAST_BOUGHT_PRODUCTS': 100,
+    # Time of minutes a user has for a purchase to undo it
     'T_ANNULLABLE_PURCHASE_M': 60,
+    # number of products to be shown in 'Häufig gekauft'
     'N_MOST_BOUGHT_PRODUCTS': 10,
+    # Number of days in the past that are used as time intervall limit to
+    # search for 'Häufig gekauft'
     'T_MOST_BOUGHT_PRODUCTS_D': 30,
+    # Number of charges that should be shown in 'Letzte Aufladungen'. A
+    # negative number means 'all users are selected'
     'N_LAST_CHARGES': 10,
+    # Time in minutes a user has for a charge to undo it
     'T_ANNULLABLE_CHARGE_M': 60,
+    # Number of receivers that should be shown in 'alle Empfänger'. A negative
+    # value means 'all users are shown'
     'N_TRANSFERS_RECEIVERS': -1,
+    # Number of transfers that should be shown in 'Letzte Überweisungen'
     'N_LAST_TRANSFERS': 10,
+    # Time in minutes a user has for a transfer to undo it
     'T_ANNULLABLE_TRANSFERS_M': 60,
 }
