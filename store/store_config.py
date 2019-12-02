da8407b4fde9ed06c3a646eaf2285969a15f9979
Simon Ruderich <ruderich@cs.fau.de>
2019-12-02 12:55:45 +0100

    store_config: remove unused option T_LAST_BOUGHT_PRODUCTS_D

--- store/store_config.py
+++ store/store_config.py
@@ -2,7 +2,6 @@
 
 """
 N_LAST_BOUGHT_PRODUCTS: number of products to be shown in 'Letzte Einkäufe'
-T_LAST_BOUGHT_PRODUCTS_D: number of days in the past that are used as time intervall limit to search for 'Letzte Einkäufe'
 T_ANNULLABLE_PURCHASE_M: time of minutes a user has for a purchase to undo it
 N_MOST_BOUGHT_PRODUCTS: number of products to be shown in 'Häufig gekauft'
 T_MOST_BOUGHT_PRODUCTS_D: number of days in the past that are used as time intervall limit to search for 'Häufig gekauft'
@@ -16,7 +15,6 @@ T_ANNULLABLE_TRANSFERS_M: time in minutes a user has for a transfer to undo it
 """
 KAFFEEKASSE = {
     'N_LAST_BOUGHT_PRODUCTS': 100,
-    'T_LAST_BOUGHT_PRODUCTS_D': 30,
     'T_ANNULLABLE_PURCHASE_M': 60,
     'N_MOST_BOUGHT_PRODUCTS': 10,
     'T_MOST_BOUGHT_PRODUCTS_D': 30,
