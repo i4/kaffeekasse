i4/kaffeekasse

commit 61efe6a9fb84e777c1c840079a0419ac0c93a482

models: Charge: add comment field

store/models.py
@@ -124,6 +124,7 @@ class Charge(models.Model):
     time_stamp = models.DateTimeField(auto_now=True)
     amount = models.DecimalField(max_digits=6, decimal_places=2)
     annulled = models.BooleanField(default=False)
+    comment = models.TextField()
 
 
 class Purchase(models.Model):

store/admin.py
@@ -131,7 +131,7 @@ class ProductIdentifierAdmin(admin.ModelAdmin):
 
 @admin.register(models.Charge)
 class ChargeAdmin(AppendOnlyModelAdmin):
-    list_display = ('time_stamp', 'user', 'amount', 'annulled')
+    list_display = ('time_stamp', 'user', 'amount', 'annulled', 'comment')
     # "annulled" to prevent enabling it when adding new objects
     readonly_fields = ('time_stamp', 'annulled')
 

commit 5af2e1f06c542067aea545e2131eef85911f50bc

models: Charge: permit blank comments
This restores the ability to perform manual charges. Previously they
failed with "django.core.exceptions.ValidationError: {'comment': ['This
field cannot be blank.']}". Broken in 61efe6a ("models: Charge: add
comment field", 2019-12-05)

store/models.py
@@ -139,7 +139,7 @@ class Charge(models.Model):
     time_stamp = models.DateTimeField(auto_now=True)
     amount = models.DecimalField(max_digits=6, decimal_places=2)
     annulled = models.BooleanField(default=False)
-    comment = models.TextField()
+    comment = models.TextField(blank=True)
 
 
 class Purchase(models.Model):